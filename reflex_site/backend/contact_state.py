a714edfd4c1f4961f2d5ad5bdc18e206e1dd8b4d
Alex <al.kovalyov@gmail.com>
2024-11-13 13:13:10 -0600

    add confirmation message after contact message submitted

--- reflex_site/backend/contact_state.py
+++ reflex_site/backend/contact_state.py
@@ -1,9 +1,11 @@
 import reflex as rx
 from .models import ContactMessage
 from datetime import datetime, timezone
+from ..backend import MainState
+from ..navigation import routes
 
 
-class ContactState(rx.State):
+class ContactState(MainState):
 
     def handle_submit(self, form_data: dict):
         form_data["created_at"] = datetime.now(timezone.utc)
@@ -11,5 +13,7 @@ class ContactState(rx.State):
             db_entry = ContactMessage(**form_data)
             session.add(db_entry)
             session.commit()
-
-        # TODO send confirmation message
+            self.set_pending_callout(
+                "Your message has been submitted successfully.", False
+            )
+            return rx.redirect(routes.HOME_ROUTE)
